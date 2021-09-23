d06f48bf7bb9d2fdac2c9a817c29e44d816e5968
Lano-23 <plangton74@gmail.com>
2021-09-23 14:45:28 +0100

    Colour points purple

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,7 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(aes = "blue") +
+  geom_point(aes = "purple") +
   ggtitle("Weather points in Sydney")
 
 # Save the plot in graphics/
